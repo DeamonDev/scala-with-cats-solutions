f8eea7aeab590847cb11084cfd04c50429f15c90
piotrek <deamondev@gmail.com>
2021-08-24 20:45:28 +0200

    show

--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -1,8 +1,31 @@
 package sandbox
 
-import cats.instances.string._
-import cats.syntax.semigroup._
+import cats._
+import cats.implicits._
+import java.util.Date
 
 object Main extends App {
-  println("Hello " |+| "Cats!")
+
+  val showInt: Show[Int] = Show.apply[Int]
+  val showString: Show[String] = Show.apply[String]
+
+  val intAsString: String = showInt.show(42)
+  val stringAsString: String = showString.show("Omi the cat!")
+  
+
+  implicit val dateShow: Show[Date] = 
+    new Show[Date] { 
+      def show(date: Date): String = 
+        s"${date.getTime}ms since the epoch."
+    }
+
+  println(new Date().show)
+  // 1629830517073ms since the epoch.
+  
+  /*
+    implicit val dateShow: Show[Date] = 
+      Show.show(date => s"{date.getTime}ms since the spoch.")
+  */
+
+
 }
